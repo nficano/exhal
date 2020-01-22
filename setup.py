454740bd88b59921bfd3a81b10ff2ef85bf27515
Nick Ficano <nficano@gmail.com>
2020-01-22 07:49:13 -0500

    Bumpversion

--- setup.py
+++ setup.py
@@ -52,7 +52,7 @@ class UploadCommand(Command):
 
 setup(
     name="exhal",
-    version="1.0.0",
+    version="1.0.1",
     author="Nick Ficano",
     author_email="nficano@gmail.com",
     packages=["exhal"],
